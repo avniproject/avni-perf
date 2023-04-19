3813b8c3904df49b00151cd2eaf255550c1c38e0
Joy A <itsjoy@gmail.com>
2023-04-19 11:40:39 +0530

    #0 | Update SyncDetail to include encounterOrEnrolmentType

--- src/gatling/java/org/avni/models/SyncDetail.java
+++ src/gatling/java/org/avni/models/SyncDetail.java
@@ -6,13 +6,15 @@ public class SyncDetail {
     public String loadedSince;
     public String entityTypeUuid;
     public boolean approvalStatusType;
+    public boolean encounterOrEnrolmentType;
 
-    public SyncDetail(String uuid, String entityName, String loadedSince, String entityTypeUuid, boolean approvalStatusType) {
+    public SyncDetail(String uuid, String entityName, String loadedSince, String entityTypeUuid, boolean approvalStatusType, boolean encounterOrEnrolmentType) {
         this.uuid = uuid;
         this.entityName = entityName;
         this.loadedSince = loadedSince;
         this.entityTypeUuid = entityTypeUuid;
         this.approvalStatusType = approvalStatusType;
+        this.encounterOrEnrolmentType = encounterOrEnrolmentType;
     }
 
     public SyncDetail() {
